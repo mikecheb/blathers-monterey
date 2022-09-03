228c5ff6d021ea65afc18d90680813b946933893
Mike Chen <mikeche@justin.tv>
2022-09-02 19:16:07 -0700

    Finish data entry

---
+++ src/data.js
@@ -0,0 +1,332 @@
+export const DATA = {
+  clownfish: {
+    id: "clownfish",
+    name: "Clown Fish",
+    icon: "NH-Icon-clownfish.png",
+    joke: "I caught a clown fish! How many can fit in a carfish?",
+    blurb: "Few can deny the delightful qualities of the clown fish. These vibrant fishes make their home among venomous anemones. It's a mutually beneficial arrangement. The clown fish finds safety in the anemone's wriggling arms, as well as leftover scraps of food. In return, clown fish ward off parasites and predators for the anemone. Thus these colorful fish remind us of the importance of maintaining good cheer among our roommates.",
+  },
+  anchovy: {
+    id: "anchovy",
+    name: "Anchovy",
+    icon: "NH-Icon-anchovy.png",
+    joke: "I caught an anchovy! Stay away from my pizza!",
+    blurb: <span>There are more than 140 species of anchovy, but they do all have some things in common. They are all small and feed by simply swimming with heir mouths open to filter food particles from the sea. <small>In my sleepier moments, I sometimes wish I could do that...</small></span>,
+  },
+  giantisopod: {
+    id: "giantisopod",
+    name: "Giant Isopod",
+    icon: "Giant_Isopod_NH_Icon.png",
+    joke: <span><big>GAAAAH!</big> I got a giant isopod! It's the crustodian of the sea.</span>,
+    blurb: <span>Growing up to neatly 20 inches, the sea-dwelling giant isopod is the world's largest cousin of the pill bug. <small>Twenty inches?! Twenty times as revolting, I say!</small> Making matters worse for those of us who find the pill bug repulsive rather than "cute"... The giant isopod can swim shockingly fast and is known for its voracious appetite. In fact, it's most happy when dining on the carcasses of animals that have sunk to the bottom of the sea! This behavior has earned it a nickname..."the vacuum cleaner of the deep." <small>Why must nature play such awful pranks on me?</small></span>,
+  },
+  hammerhead: {
+    id: "hammerhead",
+    name: "Hammerhead Shark",
+    icon: "Hammerhead_Shark_NH_Icon.png",
+    joke: "I caught a hammerhead shark! I hit the nail on the head!",
+    blurb: "The hammerhead shark is known for its distinctive, hammer-shaped head, hence the name. Er, obviously. Anyhoot, this oddly shaped head allows the beast to see 360 degrees around itself, from top to bottom. Imagine being able to see so widely! You could check for untied shoelaces AND imminent rain simultaneously!",
+  },
+  moonjelly: {
+    id: "moonjelly",
+    name: "Moon Jellyfish",
+    icon: "NH-Icon-moonjellyfish.png",
+    joke: "I got a moon jellyfish! Now to find a sun peanutbutterfish.",
+    blurb: "Ah, the moon jellyfish...what's not to love about this pretty floating flower of the sea... Well, I suppose its tentacles do give a sting when touched...but it's only a mild thing to the likes of us. Other creatures may not be so fond of this translucent beauty. Though who can blame them, really? Moon jellyfish have stinging cells called cnidocytes that kill fish and other small critters that float by. And they use their \"oral arms\" to pull these morsels into their mouths and stomachs. Indeed, one need but observe the moon jellyfish to see how brutal AND beautiful nature can be.",
+  },
+  abalone: {
+    id: "abalone",
+    name: "Abalone",
+    icon: "Abalone_NH_Icon.png",
+    joke: "I got an abalone! Why do I want a sandwich now?",
+    blurb: "My feathers, but the abalone is a most deceptive sea snail, wot wot! After seeing its dull brown shell from the outside, one would think it quite plain. But take a gander inside, and you'll see that the abalone's home is a shimmering beauty to behold! The inner layer of the shell is made of \"nacre,\" or \"mother-of-pearl,\" and, hoo, what a dazzling iridescent hue! Let this be a lesson! You mustn't judge a sea snail by the outside of its shell. Judge by the inside of its shell instead.",
+  },
+  mussel: {
+    id: "mussel",
+    name: "Mussel",
+    icon: "Mussel_NH_Icon.png",
+    joke: "I got a mussel! It's been working out.",
+    blurb: `Did you know that the mussel has a beard?! Hoo! It's true! Well, partially true, anyway. Mussels have a special gland that secretes a thing called "byssus" or "byssal thread." They use these tough beard-like fibers to anchor themselves to rocks and keep themselves in place. So, you see, the mussel's beard is functional rather than merely stylish. Then again, perhaps all beards should be this way.`,
+  },
+  hermitcrab: {
+    id: "hermitcrab",
+    name: "Hermit Crab",
+    icon: "NH-Icon-hermitcrab.png",
+    joke: "I caught a hermit crab! I think it wanted to be left alone!",
+    blurb: <span>The hermit crab is not an insect... Though it most certainly resembles one! Blech! It looks much like a spider—what with its creepy eyes and crawly legs—but is, in fact, a crustacean. As such, the hermit crab has 10 legs and also wears a shell. But it doesn't grow this shell itself. It slips its soft body into shells left behind by snails, you see, and moves into ever-larger ones as it grows. <small>Talk about a strange way to make a home.</small></span>,
+  },
+  dab: {
+    id: "dab",
+    name: "Dab",
+    icon: "NH-Icon-dab.png",
+    joke: "I caught a dab! Not bad!",
+    blurb: `The dab is a flatfish that bears some resemblance to the olive flounder, though their mouths are different. Personally, I am morbidly fascinated by each fish's eye placement. The dab's eyes are both on its right side, while the olive flounder's are on the left. These fish spend lots of time lying on their sides in the sand, so the odd eye placement makes some sense... But it's-how can I put this? Creepy? Viscerally disturbing? Profoundly upsetting to fans of symmetry?`,
+  },
+  giantclam: {
+    id: "giantclam",
+    name: "Gigas Giant Clam",
+    icon: "Gigas_Giant_Clam_NH_Icon.png",
+    joke: "I got a gigas giant clam! It's kind of a big deal.",
+    blurb: `Gigas giant clams live in warm-water coral reefs and are the biggest bivalves in the world. Growing as wide as six feet and weighing up to 600 pounds, these clams can live to be 100 years old. And yes, these mighty creatures look as though they could eat a swimmer whole. But they eat plankton instead and get valuable nutrients from an algae they live in symbiosis with. Indeed, these enormous gents get all the sustenance they need by lying face up with their shells half-open. If such was my life, I'd be happy as a clam too.`,
+  },
+  seabass: {
+    id: "seabass",
+    name: "Sea Bass",
+    icon: "NH-Icon-seabass.png",
+    joke: "I caught a sea bass! No, wait—it's at least a C+!",
+    blurb: `Sea bass is a name given to a variety of different species of saltwater fish. They are a varied bunch with some as small as four inches and some as staggeringly ginormous as eight feet! "Sea bass" is a bit pedestrian, though. Many species have better names, such as "redbanded perch." Or the delightfully whimsical dusky grouper! Or the potato cod! WILL NO ONE THINK OF THE PINK MAOMAO?!`,
+  },
+  sturgeon: {
+    id: "sturgeon",
+    name: "Sturgeon",
+    icon: "NH-Icon-sturgeon.png",
+    joke: "I caught a sturgeon! Wonder if it can perform sturgery...",
+    blurb: `The sturgeon is a large and long-lived fish which has changed little in the last 300 million years. One curious behavior of theirs is leaping high out of the water and falling back in on their sides. The smacking sounds of their re-entry can be heard for up to half a mile away, likely more underwater! No one knows why they do it, but I like to think it's their impression of bread popping out of a toaster!`,
+  },
+  seacucumber: {
+    id: "seacucumber",
+    name: "Sea Cucumber",
+    icon: "Sea_Cucumber_NH_Icon.png",
+    joke: "I got a sea cucumber! It's a bit cumbersome...",
+    blurb: `The sea cucumber, much like its relatives the sea urchin and sea star, has no heart nor brain. However, unlike its relatives, it has no shell nor spikes with which to protect itself either. So, how does a softy such as this stay safe in the open ocean? Brace yourself, I say... When threatened, sea cucumbers jettison their innards...out of their backsides! Hoo! Gross but true! The sticky, stringy stuff ensnares predators, allowing the cuke to make its escape and grow its insides back. One might say...the sea cucumber is a GUTSY creature, indeed. Hoo hoo, hee hee! Do forgive me!`,
+  },
+  seaweed: {
+    id: "seaweed",
+    name: "Seaweed",
+    icon: "NH-Icon-seaweed.png",
+    joke: "I got some seaweed! I couldn't kelp myself.",
+    blurb: <span>
+      Let it be known that seaweed is a misnomer of the highest order! That is, it is not a noxious weed so much as it is a marine algae most beneficial to life on land and sea. Seaweed, you see, provides essential habitat and food for all manner of marine creatures. And it creates a great deal of the oxygen we land lovers love to breathe too, hoo! And yet, I can't help but shudder when the slimy stuff touches my toes during a swim. <small>Hoot! The horror!"</small>
+    </span>
+  },
+  acornbarnacle: {
+    id: "acornbarnacle",
+    name: "Acorn Barnacle",
+    icon: "Acorn_Barnacle_NH_Icon.png",
+    joke: "I got an acorn barnacle! Will it grow into an oak barnacle?",
+    blurb: `Do not mistake the acorn barnacle for a relative of the clam. Tut-tut, mollusks they are not. Barnacles are cousins to crabs and lobsters, you see. Though the family resemblance is hard to spot. Acorn barnacles may also seem to live a stationary life, what with being attached to rocks and such. But truth be told, baby barnacles are avid travelers, drifting shell-less about the sea before settling down. Once they find a good rock to affix themselves to, they float free no more. Which begs the question... Do adult acorn barnacles ever look out across the open sea...and long for youthful adventure once more?`
+  },
+  spidercrab: {
+    id: "spidercrab",
+    name: "Spider Crab",
+    icon: "NH-Icon-spidercrab.png",
+    joke: "I got a spider crab! Water bugs can rest easy.",
+    blurb: `Hoooot hooot, I do so hope to not hyperventilate as I regale you with the spider crab's qualities. But I would be remiss if I did not point out that this, the largest crab on earth...looks like a GIANT SPIDER! Arachnophobia aside...this crab is a magnificent creature to be sure. Though they can grow up to 12 feet from claw to claw and weigh up to 40 pounds, they're said to be docile. In fact, these gentle giants love to hide, attaching seaweed and sponges to their shell as camouflage. My feathers, if only I could unsee the SPIDER in the spider crab...I'm sure I'd find it charming as can be.`
+  },
+  sweetshrimp: {
+    id: "sweetshrimp",
+    name: "Sweet Shrimp",
+    icon: "NH-Icon-sweetshrimp.png",
+    joke: "I got a sweet shrimp! It's being very considerate.",
+    blurb: `The sweet shrimp spends the first few years of its life as a male. It then transitions into a female as it ages. Interestingly, it is hardly alone in the type of development cycle. Indeed, a great variety of marine creatures can change their biological sex as they age. Clown fish, some sea stars, and sea bass are among them. Hoo hoo, the ocean is full of a great many wonders. Huzzah to that, I say!`
+  },
+  morayeel: {
+    id: "morayeel",
+    name: "Moray Eel",
+    icon: "NH-Icon-morayeel.png",
+    joke: "I caught a moray eel! When you're in love, that's a moray!",
+    blurb: `Despite its rather frightening features, the moray eel is really a very cowardly fish. It lives its life hiding in caves, and will only attack if you're looking for a fight. You've heard of a "bark that is worse than the bite"? Well, the moray eel has a face that's worse than its fight!`
+  },
+  tuna: {
+    id: "tuna",
+    name: "Tuna",
+    icon: "NH-Icon-tuna.png",
+    joke: "I caught a tuna! It's a little off-key!",
+    blurb: `The tuna is a large ocean fish that can reach upwards of nine feet long. Besides its great size, it's notable for continuing to swim even when it's sleeping! Well, to be fair, if it doesn't keep swimming, it won't be able to breathe and will of course die... But sleep should be restful, eh wot! If I were the tuna I would feel very hard done by!`
+  },
+  seaurchin: {
+    id: "seaurchin",
+    name: "Sea Urchin",
+    icon: "NH-Icon-seaurchin.png",
+    joke: "I got a sea urchin! Wasn't even 'earchin for it.",
+    blurb: `Ah, the sea urchin...the unabashed goth of the ocean floor! Indeed, with its mane of dark-hued spikes, it looks for all the world as if it might front a punk-rock band. Perhaps its more demure relatives—the sea cucumber and the sea star—disapprove of its outlandish look? 'Twould certainly explain why sea stars gobble up sea urchins every chance they get! But worry not for our spiky friend. Its venomous spines are an effective deterrent to many a predator. And then there are its sticky "tube feet," which it uses to maneuver out of harm's way. TUBE feet, I say! What will this salty rebel think of next?`
+  },
+  ray: {
+    id: "ray",
+    name: "Ray",
+    icon: "NH-Icon-ray.png",
+    joke: "I caught a ray! A few more and I'll have a tan!",
+    blurb: `The ray is an unusually shaped fish with a flat body and a long body, somewhat related to sharks. Seen from the bottom, the mouth on its stomach appears to be smiling, which is objectively adorable. But be cautious—unlike most cute animals, some rays are extremely venomous. BEWARE THE SMILE.`
+  },
+  squid: {
+    id: "squid",
+    name: "Squid",
+    icon: "NH-Icon-squid.png",
+    joke: "I caught a squid! I had an inkling I might!",
+    blurb: `Well, first off, squids are members of the mollusk family, just like the octopus. Many mollusks like snails and clams have shells, but the squid's body is almost completely soft. The exception is a long, narrow bit of hard material going through its main body-the last remnant of its shell. Of course, having your shell INSIDE is not a best practice. Perhaps they lost the owner's manual!`
+  },
+  octopus: {
+    id: "octopus",
+    name: "Octopus",
+    icon: "NH-Icon-octopus.png",
+    joke: "I got an octopus! It can give four hugs at once!",
+    blurb: `With its eight limbs, three hearts and surprisingly big brain, the octopus is quite the wonder! It is also an escape artist like no other! You see, the octopus has specialized skin cells that can change color and texture with lightning speed. Thus it can look for all the world like a patch of seabed one minute...and a pile of rocks the next! But the deception doesn't stop there, wot wot. When attacked by a predator, the octopus releases a cloud of ink and–POOF–makes its escape! Let's just call the octopus what it is, shall we? The ninja of the deep!`
+  },
+  seastar: {
+    id: "seastar",
+    name: "Sea Star",
+    icon: "NH-Icon-seastar.png",
+    joke: "I got a sea star! Maybe it'll give me an autograph!",
+    blurb: `The sea star may appear to have five feet...but those are, in fact, its arms. And though five-armed sea stars are the most common, there are species with 20 or even 40 arms out there! Ah, but the fascinating appendage facts do not end there, wot wot! You see, upon each sea star's arm sprout many dozens of tiny feet. Yes, FEET! They use these suction-cupped feet not just to move about...but to...eh...grab hold of dinner guests as well. Finally, it should be noted that when a sea star loses an arm, it can grow it back. More impressive yet, a severed sea star arm...can sometimes grow an entire sea star body back!`
+  },
+  seaanemone: {
+    id: "seaanemone",
+    name: "Sea Anemone",
+    icon: "NH-Icon-seaanemone.png",
+    joke: "I got a sea anemone! The enemy of my anemone is my afrenome!",
+    blurb: `Medusa herself would be most impressed by the sea anemone. This pretty predator loves to wave its colorful flowing locks about. But those gorgeous tresses are, in fact, deadly tentacles surrounding a hungry mouth. Triggered by the slightest touch, these tentacles harpoon victims with neurotoxin. The sea anemone then pulls the helpless, hapless prey into its mouth for a spot of lunch! I say, let this be a lesson! Never, ever make an enemy...out of an anemone!`
+  }
+};
+
+export const EXHIBITS = [
+  "Giant Pacific Octopus",
+  "Kelp Forest",
+  "Monterey Bay Habitats",
+  "Sandy Shore & Aviary",
+  "Rocky Shore Touch Pools",
+  "Splash Zone & Penguins",
+  "The Open Sea",
+  "Into the Deep",
+  "Tentacles",
+  "Sea Otters",
+  "Life on the Bay",
+];
+
+export const MAPPINGS = {
+  "Giant Pacific Octopus": [{
+    id: "seaurchin",
+    variants: "Fragile pink sea urchin, purple sea urchin",
+  }, {
+    id: "octopus",
+    variants: "Giant Pacific octopus",
+  }],
+  "Kelp Forest": [{
+    id: "anchovy",
+    variants: "Northern anchovy",
+  }, {
+    id: "hermitcrab",
+  }, {
+    id: "seabass",
+    variants: "Giant sea bass",
+  }, {
+    id: "seacucumber",
+    variants: "Warty sea cucumber",
+  }, {
+    id: "seaweed",
+    variants: "Bull kelp, giant kelp"
+  }, {
+    id: "octopus",
+    variants: "Red octopus",
+  }, {
+    id: "seastar",
+    variants: "Bat star",
+  }, {
+    id: "seaanemone",
+    variants: "Giant green anemone",
+  }],
+  "Monterey Bay Habitats": [{
+    id: "dab",
+    variants: "Sanddab",
+  }, {
+    id: "seabass",
+    variants: "Giant sea bass",
+  }, {
+    id: "sturgeon",
+    variants: "White sturgeon",
+  }, {
+    id: "acornbarnacle",
+  }, {
+    id: "sweetshrimp",
+    variants: "Spot prawn"
+  }, {
+    id: "ray",
+    variants: "Bat ray",
+  }, {
+    id: "seastar",
+    variants: "Bat star, spiny brittle star",
+  }, {
+    id: "seaanemone",
+    variants: "Fish-eating anemone, tube anemone",
+  }],
+  "Sandy Shore & Aviary": [{
+    id: "ray",
+    variants: "Bat ray",
+  }],
+  "Rocky Shore Touch Pools": [{
+    id: "abalone",
+  }, {
+    id: "mussel",
+    variants: "California mussel",
+  }, {
+    id: "hermitcrab"
+  }, {
+    id: "seaweed",
+    variants: "Dead man's fingers"
+  }, {
+    id: "acornbarnacle",
+  }, {
+    id: "seaurchin",
+    variants: "Purple sea urchin",
+  }, {
+    id: "seastar",
+    variants: "Bat star",
+  }, {
+    id: "seaanemone",
+    variants: "Aggregating anemone, giant green anemone",
+  }],
+  "Splash Zone & Penguins": [{
+    id: "clownfish",
+    variants: "Clownfish",
+  }, {
+    id: "hermitcrab",
+  }, {
+    id: "giantclam",
+    variants: "Giant Clam",
+  }, {
+    id: "morayeel",
+    variants: "California moray, zebra moray"
+  }, {
+    id: "seaanemone",
+    variants: "Carpet anemone, giant green anemone",
+  }],
+  "The Open Sea": [{
+    id: "moonjelly",
+    variants: "Moon jelly"
+  }, {
+    id: "anchovy",
+    variants: "Northern anchovy",
+  }, {
+    id: "hammerhead",
+    variants: "Scalloped hammerhead shark",
+  }, {
+    id: "tuna",
+    variants: "Pacific bonito, yellowfin tuna"
+  }, {
+    id: "ray",
+    variants: "Pelagic stingray",
+  }],
+  "Into the Deep": [{
+    id: "giantisopod",
+  }, {
+    id: "seacucumber",
+    variants: "Glowing sea cucumber",
+  }, {
+    id: "spidercrab",
+    variants: "Japanese spider crab"
+  }, {
+    id: "seastar",
+    variants: "Basket star, feather star, fire star"
+  }, {
+    id: "seaanemone",
+    variants: "Apple anemone, pom-pom anemone",
+  }],
+  "Tentacles": [{
+    id: "squid",
+    variants: "Bigfin reef squid",
+  }, {
+    id: "octopus",
+    variants: "Giant Pacific octopus, two-spot octopus",
+  }],
+  "Sea Otters": [],
+  "Life on the Bay": [],
+};
